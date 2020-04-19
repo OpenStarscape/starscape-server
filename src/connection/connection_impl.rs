4861ef4ccde04b394090a56c1125793a3a53b1f4
William Wold <wm@wmww.sh>
2020-04-19 06:02:39 -0400

    Add Value::List

--- src/connection/connection_impl.rs
+++ src/connection/connection_impl.rs
@@ -46,10 +46,35 @@ impl ConnectionImpl {
     /// Entities and collections containing them need to be resolved to object IDs
     fn resolve_value<'a>(objects: &mut ObjectMap, unresolved_value: &'a Value) -> Option<Value> {
         match unresolved_value {
+            // if the value is an entity, we need to look up the connection-specific object ID
             Value::Entity(entity) => Some(Value::Integer(match objects.get_object(*entity) {
                 Some(o) => o,
                 None => objects.register_entity(*entity),
             } as i64)),
+            // if this value is a list, it could contain entities that should be resolved
+            Value::List(list) => {
+                // Search throug the list, looking for an element that need to be resolved
+                match list.iter().enumerate().find_map(|(i, element)| {
+                    Self::resolve_value(objects, element).map(|resolved| (i, resolved))
+                }) {
+                    // if we find one
+                    Some((i, first_resolved)) => {
+                        // clone the first part of the vec that didn't have any resolvable elements
+                        let first_part = list.iter().take(i).cloned();
+                        // insert the element we resolved
+                        let with_first_resolved = first_part.chain(std::iter::once(first_resolved));
+                        // and resolve or clone the rest of the vec as needed
+                        let with_rest =
+                            with_first_resolved.chain(list[i + 1..].iter().map(|element| {
+                                Self::resolve_value(objects, element)
+                                    .unwrap_or_else(|| element.clone())
+                            }));
+                        Some(Value::List(with_rest.collect()))
+                    }
+                    // otherwise, no elements need to be resolved
+                    None => None,
+                }
+            }
             _ => None,
         }
     }
@@ -176,7 +201,7 @@ mod tests {
                 .lock()
                 .unwrap()
                 .get_object(self.entities[0])
-                .unwrap()
+                .expect("failed to look up object")
         }
 
         fn lookup_obj_ids(&self) -> Vec<ObjectId> {
@@ -188,7 +213,7 @@ mod tests {
                         .lock()
                         .unwrap()
                         .get_object(*e_key)
-                        .unwrap()
+                        .unwrap_or(0)
                 })
                 .collect()
         }
@@ -253,6 +278,106 @@ mod tests {
                 (test.obj_id, "foo".to_owned(), Value::Integer(obj_0 as i64)),
                 (test.obj_id, "bar".to_owned(), Value::Integer(obj_0 as i64))
             ]
+        );
+    }
+
+    #[test]
+    fn resolves_list_of_entites_to_object_ids() {
+        let test = Test::new();
+        test.conn
+            .property_changed(test.entity, "foo", &test.entities.clone().into())
+            .expect("Error updating property");
+        let obj_ids = test.lookup_obj_ids();
+        assert_eq!(
+            test.proto.borrow().log,
+            vec![(test.obj_id, "foo".to_owned(), obj_ids.into())]
+        );
+    }
+
+    #[test]
+    fn resolves_list_with_entity_and_other_stuff() {
+        let test = Test::new();
+        test.conn
+            .property_changed(
+                test.entity,
+                "foo",
+                &Value::List(vec![
+                    Value::Integer(42),
+                    Value::Entity(test.entities[0]),
+                    Value::Null,
+                ]),
+            )
+            .expect("Error updating property");
+        let obj_0 = test.lookup_obj_0();
+        assert_eq!(
+            test.proto.borrow().log,
+            vec![(
+                test.obj_id,
+                "foo".to_owned(),
+                Value::List(vec![
+                    Value::Integer(42),
+                    Value::Integer(obj_0 as i64),
+                    Value::Null
+                ])
+            )],
+        );
+    }
+
+    #[test]
+    fn resolves_list_with_single_entity() {
+        let test = Test::new();
+        test.conn
+            .property_changed(
+                test.entity,
+                "foo",
+                &Value::List(vec![Value::Entity(test.entities[0])]),
+            )
+            .expect("Error updating property");
+        let obj_0 = test.lookup_obj_0();
+        assert_eq!(
+            test.proto.borrow().log,
+            vec![(
+                test.obj_id,
+                "foo".to_owned(),
+                Value::List(vec![Value::Integer(obj_0 as i64),])
+            )],
+        );
+    }
+
+    #[test]
+    fn resolves_nested_lists_with_entities() {
+        let test = Test::new();
+        test.conn
+            .property_changed(
+                test.entity,
+                "foo",
+                &Value::List(vec![
+                    Value::List(vec![Value::List(vec![
+                        Value::Entity(test.entities[1]),
+                        Value::Entity(test.entities[2]),
+                        Value::Null,
+                    ])]),
+                    Value::Integer(42),
+                    Value::Entity(test.entities[0]),
+                ]),
+            )
+            .expect("Error updating property");
+        let obj_ids = test.lookup_obj_ids();
+        assert_eq!(
+            test.proto.borrow().log,
+            vec![(
+                test.obj_id,
+                "foo".to_owned(),
+                Value::List(vec![
+                    Value::List(vec![Value::List(vec![
+                        Value::Integer(obj_ids[1] as i64),
+                        Value::Integer(obj_ids[2] as i64),
+                        Value::Null,
+                    ]),]),
+                    Value::Integer(42),
+                    Value::Integer(obj_ids[0] as i64),
+                ]),
+            )],
         );
     }
 }
