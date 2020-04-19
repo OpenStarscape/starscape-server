6dc8768d33736fe0e54fd00608e599c4001f6bbc
William Wold <wm@wmww.sh>
2020-04-19 00:00:39 -0400

    Split UpdateSource struct off from store

--- src/plumbing/store.rs
+++ src/plumbing/store.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
 use std::ops::Deref;
-use std::sync::RwLock;
 
+use super::UpdateSource;
 use crate::state::{PendingUpdates, PropertyKey};
 
 /// A value that can be connected to 0, 1 or more properties
@@ -9,17 +9,14 @@ use crate::state::{PendingUpdates, PropertyKey};
 /// Property keys are stored until it is time to dispatch all updates
 pub struct Store<T: PartialEq> {
     inner: T,
-    // TODO: use an atomic bool to more quickly check if watchers is empty?
-    /// The keys of watchers that want to be updated when value changes
-    /// Is conceptually a set, but since length is almost always 0 or 1 use a low cost vec
-    connections: RwLock<Vec<PropertyKey>>,
+    update_source: UpdateSource,
 }
 
 impl<T: PartialEq> Store<T> {
     pub fn new(inner: T) -> Self {
         Self {
             inner,
-            connections: RwLock::new(Vec::new()),
+            update_source: UpdateSource::new(),
         }
     }
 
@@ -31,34 +28,16 @@ impl<T: PartialEq> Store<T> {
     pub fn set(&mut self, updates: &PendingUpdates, value: T) {
         if self.inner != value {
             self.inner = value;
-            let conduits = self.connections.read().unwrap();
-            if conduits.len() > 0 {
-                let mut pending_updates =
-                    updates.write().expect("Error writing to pending updates");
-                pending_updates.extend(conduits.iter().cloned());
-            }
+            self.update_source.send_updates(updates);
         }
     }
 
     pub fn connect(&self, target: PropertyKey) -> Result<(), Box<Error>> {
-        let mut connections = self.connections.write().unwrap();
-        if connections.contains(&target) {
-            Err(format!("already connected to {:?}", target).into())
-        } else {
-            connections.push(target);
-            Ok(())
-        }
+        self.update_source.connect(target)
     }
 
     pub fn disconnect(&self, target: PropertyKey) -> Result<(), Box<Error>> {
-        let mut connections = self.connections.write().unwrap();
-        match connections.iter().position(|key| *key == target) {
-            None => Err(format!("{:?} is not connected", target).into()),
-            Some(i) => {
-                connections.swap_remove(i);
-                Ok(())
-            }
-        }
+        self.update_source.disconnect(target)
     }
 }
 
@@ -75,16 +54,7 @@ mod tests {
     use super::*;
     use crate::state::mock_keys;
     use std::collections::HashSet;
-
-    #[test]
-    fn can_update_without_connected_properties() {
-        let mut store = Store::new(7);
-        let pending = RwLock::new(HashSet::new());
-        assert_eq!(*store, 7);
-        store.set(&pending, 5);
-        assert_eq!(*store, 5);
-        assert_eq!(pending.read().unwrap().len(), 0);
-    }
+    use std::sync::RwLock;
 
     #[test]
     fn updates_connected_property_when_changed() {
@@ -98,21 +68,6 @@ mod tests {
     }
 
     #[test]
-    fn updates_multiple_connected_properties() {
-        let mut store = Store::new(7);
-        let pending = RwLock::new(HashSet::new());
-        let props = mock_keys(2);
-        props
-            .iter()
-            .for_each(|p| store.connect(*p).expect("connecting failed"));
-        store.set(&pending, 5);
-        assert_eq!(pending.read().unwrap().len(), 2);
-        props
-            .iter()
-            .for_each(|p| assert!(pending.read().unwrap().contains(p)));
-    }
-
-    #[test]
     fn does_not_update_property_when_set_to_same_value() {
         let mut store = Store::new(7);
         let pending = RwLock::new(HashSet::new());
@@ -120,14 +75,6 @@ mod tests {
         store.connect(props[0]).expect("connecting failed");
         store.set(&pending, 7);
         assert_eq!(pending.read().unwrap().len(), 0);
-    }
-
-    #[test]
-    fn connecting_same_property_twice_errors() {
-        let store = Store::new(7);
-        let props = mock_keys(1);
-        store.connect(props[0]).expect("connecting failed");
-        assert!(store.connect(props[0]).is_err());
     }
 
     #[test]
@@ -145,12 +92,4 @@ mod tests {
         assert!(!pending.read().unwrap().contains(&props[1]));
     }
 
-    #[test]
-    fn disconnecting_when_not_connected_errors() {
-        let store = Store::new(7);
-        let props = mock_keys(2);
-        assert!(store.disconnect(props[0]).is_err());
-        store.connect(props[0]).expect("connecting failed");
-        assert!(store.disconnect(props[1]).is_err());
-    }
 }
---
+++ src/plumbing/update_source.rs
@@ -0,0 +1,110 @@
+use std::error::Error;
+use std::sync::RwLock;
+
+use crate::state::{PendingUpdates, PropertyKey};
+
+pub struct UpdateSource {
+    // TODO: use an atomic bool to more quickly check if watchers is empty?
+    /// The keys of watchers that want to be updated when value changes
+    /// Is conceptually a set, but since length is almost always 0 or 1 use a low cost vec
+    connections: RwLock<Vec<PropertyKey>>,
+}
+
+impl UpdateSource {
+    pub fn new() -> Self {
+        Self {
+            connections: RwLock::new(Vec::new()),
+        }
+    }
+
+    pub fn send_updates(&self, updates: &PendingUpdates) {
+        let conduits = self.connections.read().unwrap();
+        if conduits.len() > 0 {
+            let mut pending_updates = updates.write().expect("Error writing to pending updates");
+            pending_updates.extend(conduits.iter().cloned());
+        }
+    }
+
+    pub fn connect(&self, target: PropertyKey) -> Result<(), Box<Error>> {
+        let mut connections = self.connections.write().unwrap();
+        if connections.contains(&target) {
+            Err(format!("already connected to {:?}", target).into())
+        } else {
+            connections.push(target);
+            Ok(())
+        }
+    }
+
+    pub fn disconnect(&self, target: PropertyKey) -> Result<(), Box<Error>> {
+        let mut connections = self.connections.write().unwrap();
+        match connections.iter().position(|key| *key == target) {
+            None => Err(format!("{:?} is not connected", target).into()),
+            Some(i) => {
+                connections.swap_remove(i);
+                Ok(())
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::state::mock_keys;
+    use std::collections::HashSet;
+
+    #[test]
+    fn can_update_without_connected_properties() {
+        let source = UpdateSource::new();
+        let pending = RwLock::new(HashSet::new());
+        source.send_updates(&pending);
+        assert_eq!(pending.read().unwrap().len(), 0);
+    }
+
+    #[test]
+    fn updates_multiple_connected_properties() {
+        let source = UpdateSource::new();
+        let pending = RwLock::new(HashSet::new());
+        let props = mock_keys(2);
+        props
+            .iter()
+            .for_each(|p| source.connect(*p).expect("connecting failed"));
+        source.send_updates(&pending);
+        assert_eq!(pending.read().unwrap().len(), 2);
+        props
+            .iter()
+            .for_each(|p| assert!(pending.read().unwrap().contains(p)));
+    }
+
+    #[test]
+    fn connecting_same_property_twice_errors() {
+        let source = UpdateSource::new();
+        let props = mock_keys(1);
+        source.connect(props[0]).expect("connecting failed");
+        assert!(source.connect(props[0]).is_err());
+    }
+
+    #[test]
+    fn disconnecting_stops_updates() {
+        let source = UpdateSource::new();
+        let pending = RwLock::new(HashSet::new());
+        let props = mock_keys(2);
+        props
+            .iter()
+            .for_each(|p| source.connect(*p).expect("connecting failed"));
+        source.disconnect(props[1]).expect("disconnecting failed");
+        source.send_updates(&pending);
+        assert_eq!(pending.read().unwrap().len(), 1);
+        assert!(pending.read().unwrap().contains(&props[0]));
+        assert!(!pending.read().unwrap().contains(&props[1]));
+    }
+
+    #[test]
+    fn disconnecting_when_not_connected_errors() {
+        let source = UpdateSource::new();
+        let props = mock_keys(2);
+        assert!(source.disconnect(props[0]).is_err());
+        source.connect(props[0]).expect("connecting failed");
+        assert!(source.disconnect(props[1]).is_err());
+    }
+}
