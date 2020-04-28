84933d137615d59fc61ea3a418cec03d579798da
William Wold <wm@wmww.sh>
2020-04-27 23:47:51 -0400

    network: add session channels

--- src/network/tcp_server.rs
+++ src/network/tcp_server.rs
@@ -2,6 +2,7 @@ use mio::net::{TcpListener, TcpStream};
 use mio::{Events, Poll, PollOpt, Ready, Registration, SetReadiness, Token};
 use std::error::Error;
 use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::mpsc::Sender;
 use std::sync::Arc;
 use std::thread::{spawn, JoinHandle};
 
@@ -25,7 +26,7 @@ pub struct TcpServer {
 }
 
 impl TcpServer {
-    pub fn new() -> Result<Self, Box<dyn Error>> {
+    pub fn new(new_session_tx: Sender<Box<dyn Session>>) -> Result<Self, Box<dyn Error>> {
         let addr: std::net::SocketAddr = "127.0.0.1:1212".parse()?;
         let listener = TcpListener::bind(&addr)?;
         let mio_poll_thread = new_mio_poll_thread(listener, try_to_accept_connection)?;
@@ -38,6 +39,7 @@ impl Server for TcpServer {}
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::sync::mpsc::channel;
     use std::{sync::mpsc, thread, time::Duration};
 
     const LONG_TIME: Duration = Duration::from_secs(1);
@@ -58,24 +60,39 @@ mod tests {
         });
 
         match done_rx.recv_timeout(d) {
-            Ok(_) => handle.join().expect("thread panicked"),
             Err(mpsc::RecvTimeoutError::Timeout) => panic!("thread timed out"),
-            Err(mpsc::RecvTimeoutError::Disconnected) => panic!("thread disconnected"),
+            _ => match handle.join() {
+                Ok(result) => result,
+                Err(any) => panic!("thread panicked: {:?}", any),
+            },
         }
     }
 
     #[test]
     fn can_start_and_stop_immediately() {
         panic_after(LONG_TIME, || {
-            let _server = TcpServer::new();
+            let (tx, _rx) = channel();
+            let _server = TcpServer::new(tx).expect("failed to create TCP server");
         });
     }
 
     #[test]
     fn can_start_and_stop_with_pause() {
-        panic_after(LONG_TIME, || {
-            let _server = TcpServer::new();
+        let (tx, _rx) = channel();
+        panic_after(LONG_TIME, move || {
+            let _server = TcpServer::new(tx).expect("failed to create TCP server");
             thread::sleep(SHORT_TIME);
+        });
+    }
+
+    #[test]
+    fn does_not_create_session_by_default() {
+        panic_after(LONG_TIME, || {
+            let (tx, rx) = channel();
+            let _server = TcpServer::new(tx).expect("failed to create TCP server");
+            thread::sleep(SHORT_TIME);
+            let sessions: Vec<Box<dyn Session>> = rx.try_iter().collect();
+            assert_eq!(sessions.len(), 0);
         });
     }
 }
