59591b2914c941113634d6c4ed933f4a9a16a451
William Wold <wm@wmww.sh>
2020-05-18 00:55:55 -0400

    Test server impl

--- src/server/server.rs
+++ src/server/server.rs
@@ -13,11 +13,21 @@ pub trait PropertyUpdateSink {
 
 pub trait Server {
     fn apply_updates(&mut self, state: &mut State);
+    fn number_of_connections(&self) -> usize;
     fn property_update_sink(&self) -> &dyn PropertyUpdateSink;
 }
 
 impl dyn Server {
-    pub fn new_impl() -> Box<dyn Server> {
-        Box::new(ServerImpl::new())
+    pub fn new_impl(enable_tcp: bool) -> Box<dyn Server> {
+        Box::new(ServerImpl::new(|new_session_tx| {
+            let mut listeners: Vec<Box<dyn Listener>> = Vec::new();
+            if enable_tcp {
+                match TcpListener::new(new_session_tx, None, None) {
+                    Ok(l) => listeners.push(Box::new(l)),
+                    Err(e) => eprintln!("Failed to create TCP server: {}", e),
+                };
+            }
+            listeners
+        }))
     }
 }
