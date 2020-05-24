03f7082d3152bb95da59500cdac321264ccdad5e
William Wold <wm@wmww.sh>
2020-05-24 00:36:17 -0400

    Implement ServerState for State

--- src/server/server_state.rs
+++ src/server/server_state.rs
@@ -4,6 +4,16 @@ use crate::EntityKey;
 pub trait ServerState {
     fn set(&mut self, entity: EntityKey, property: &str, value: Decodable) -> Result<(), String>;
     fn get(&self, entity: EntityKey, property: &str) -> Result<Encodable, String>;
-    fn subscribe(&mut self, entity: EntityKey, property: &str) -> Result<(), String>;
-    fn unsubscribe(&mut self, entity: EntityKey, property: &str) -> Result<(), String>;
+    fn subscribe(
+        &mut self,
+        entity: EntityKey,
+        property: &str,
+        connection: ConnectionKey,
+    ) -> Result<(), String>;
+    fn unsubscribe(
+        &mut self,
+        entity: EntityKey,
+        property: &str,
+        connection: ConnectionKey,
+    ) -> Result<(), String>;
 }
