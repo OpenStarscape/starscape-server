e08f0791766a869c34b421a06eb120b0b7ec1591
Sophie Winter <git@phie.me>
2020-10-14 23:31:29 -0700

    Rename EntityStore methods

--- src/entity/entity_store.rs
+++ src/entity/entity_store.rs
@@ -1,15 +1,10 @@
 use super::*;
 
 pub trait EntityStore {
-    fn register_property(
-        &mut self,
-        entity: EntityKey,
-        name: &'static str,
-        conduit: Box<dyn Conduit>,
-    );
-    fn new_entity(&mut self) -> EntityKey;
+    fn create_entity(&mut self) -> EntityKey;
     fn register_body(&mut self, entity: EntityKey, body: BodyKey);
     fn register_ship(&mut self, entity: EntityKey, ship: ShipKey);
+    fn create_property(&mut self, entity: EntityKey, name: &'static str, conduit: Box<dyn Conduit>);
     fn get_property(&self, entity: EntityKey, name: &str) -> Result<&Arc<dyn Property>, String>;
 }
 
