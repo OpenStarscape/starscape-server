8a2a88387da433afd7c1722e7a0cf086793608d0
William Wold <wm@wmww.sh>
2020-12-18 00:34:22 -0800

    Add class property to body

--- src/game/game.rs
+++ src/game/game.rs
@@ -37,12 +37,14 @@ impl Game {
         );
         let planet = game.state.create_entity();
         Body::new()
+            .with_class("celestial")
             .with_position(Point3::origin())
             .with_sphere_shape(6_000.0)
             .with_mass(1.0e+15)
             .install(&mut game.state, planet);
         let moon = game.state.create_entity();
         Body::new()
+            .with_class("celestial")
             .with_position(Point3::new(60_000.0, 0.0, 0.0))
             .with_velocity(Vector3::new(0.0, 0.0, 10_000.0))
             .with_sphere_shape(2_000.0)
