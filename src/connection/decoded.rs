1a9d37c945096ed8df8f904e59a24c8b1d7e6bb7
William Wold <wm@wmww.sh>
2020-12-26 20:10:29 -0800

    Support decoding arrays, options and tuples

--- src/connection/decoded.rs
+++ src/connection/decoded.rs
@@ -15,6 +15,12 @@ impl Decoded {
     }
 }
 
+impl From<Decoded> for DecodedResult<Decoded> {
+    fn from(value: Decoded) -> Self {
+        Ok(value)
+    }
+}
+
 impl From<Decoded> for DecodedResult<Vector3<f64>> {
     fn from(value: Decoded) -> Self {
         match value {
@@ -78,10 +84,13 @@ impl From<Decoded> for DecodedResult<EntityKey> {
     }
 }
 
-impl From<Decoded> for DecodedResult<Vec<Decoded>> {
+impl<T> From<Decoded> for DecodedResult<Vec<T>>
+where
+    Decoded: Into<DecodedResult<T>>,
+{
     fn from(value: Decoded) -> Self {
         match value {
-            Decoded::Array(value) => Ok(value),
+            Decoded::Array(vec) => vec.into_iter().map(Into::into).collect(),
             _ => Err(format!("{:?} is not an array", value)),
         }
     }
@@ -97,13 +106,186 @@ impl From<Decoded> for DecodedResult<()> {
     }
 }
 
-impl From<Decoded> for DecodedResult<Option<Decoded>> {
+impl<T> From<Decoded> for DecodedResult<Option<T>>
+where
+    Decoded: Into<DecodedResult<T>>,
+{
     fn from(value: Decoded) -> Self {
         if value.is_null() {
             Ok(None)
         } else {
-            Ok(Some(value))
+            Ok(Some(value.into()?))
         }
+    }
+}
+
+/// TODO: implement all the tuples with a macro
+
+impl<A> From<Decoded> for DecodedResult<(A,)>
+where
+    Decoded: Into<DecodedResult<A>>,
+{
+    fn from(value: Decoded) -> Self {
+        const LEN: usize = 1;
+        let vec = match value {
+            Decoded::Array(vec) => vec,
+            _ => {
+                return Err(format!(
+                    "can not decode {:?} as a tuple because it is not an array",
+                    value
+                ))
+            }
+        };
+        if vec.len() != LEN {
+            return Err(format!(
+                "{:?} has {} elements instead of {}",
+                vec,
+                vec.len(),
+                LEN
+            ));
+        }
+        let mut iter = vec.into_iter();
+        Ok((Into::<DecodedResult<A>>::into(iter.next().unwrap())?,))
+    }
+}
+
+impl<A, B> From<Decoded> for DecodedResult<(A, B)>
+where
+    Decoded: Into<DecodedResult<A>>,
+    Decoded: Into<DecodedResult<B>>,
+{
+    fn from(value: Decoded) -> Self {
+        const LEN: usize = 2;
+        let vec = match value {
+            Decoded::Array(vec) => vec,
+            _ => {
+                return Err(format!(
+                    "can not decode {:?} as a tuple because it is not an array",
+                    value
+                ))
+            }
+        };
+        if vec.len() != LEN {
+            return Err(format!(
+                "{:?} has {} elements instead of {}",
+                vec,
+                vec.len(),
+                LEN
+            ));
+        }
+        let mut iter = vec.into_iter();
+        Ok((
+            Into::<DecodedResult<A>>::into(iter.next().unwrap())?,
+            Into::<DecodedResult<B>>::into(iter.next().unwrap())?,
+        ))
+    }
+}
+
+impl<A, B, C> From<Decoded> for DecodedResult<(A, B, C)>
+where
+    Decoded: Into<DecodedResult<A>>,
+    Decoded: Into<DecodedResult<B>>,
+    Decoded: Into<DecodedResult<C>>,
+{
+    fn from(value: Decoded) -> Self {
+        const LEN: usize = 3;
+        let vec = match value {
+            Decoded::Array(vec) => vec,
+            _ => {
+                return Err(format!(
+                    "can not decode {:?} as a tuple because it is not an array",
+                    value
+                ))
+            }
+        };
+        if vec.len() != LEN {
+            return Err(format!(
+                "{:?} has {} elements instead of {}",
+                vec,
+                vec.len(),
+                LEN
+            ));
+        }
+        let mut iter = vec.into_iter();
+        Ok((
+            Into::<DecodedResult<A>>::into(iter.next().unwrap())?,
+            Into::<DecodedResult<B>>::into(iter.next().unwrap())?,
+            Into::<DecodedResult<C>>::into(iter.next().unwrap())?,
+        ))
+    }
+}
+
+impl<A, B, C, D> From<Decoded> for DecodedResult<(A, B, C, D)>
+where
+    Decoded: Into<DecodedResult<A>>,
+    Decoded: Into<DecodedResult<B>>,
+    Decoded: Into<DecodedResult<C>>,
+    Decoded: Into<DecodedResult<D>>,
+{
+    fn from(value: Decoded) -> Self {
+        const LEN: usize = 4;
+        let vec = match value {
+            Decoded::Array(vec) => vec,
+            _ => {
+                return Err(format!(
+                    "can not decode {:?} as a tuple because it is not an array",
+                    value
+                ))
+            }
+        };
+        if vec.len() != LEN {
+            return Err(format!(
+                "{:?} has {} elements instead of {}",
+                vec,
+                vec.len(),
+                LEN
+            ));
+        }
+        let mut iter = vec.into_iter();
+        Ok((
+            Into::<DecodedResult<A>>::into(iter.next().unwrap())?,
+            Into::<DecodedResult<B>>::into(iter.next().unwrap())?,
+            Into::<DecodedResult<C>>::into(iter.next().unwrap())?,
+            Into::<DecodedResult<D>>::into(iter.next().unwrap())?,
+        ))
+    }
+}
+
+impl<A, B, C, D, E> From<Decoded> for DecodedResult<(A, B, C, D, E)>
+where
+    Decoded: Into<DecodedResult<A>>,
+    Decoded: Into<DecodedResult<B>>,
+    Decoded: Into<DecodedResult<C>>,
+    Decoded: Into<DecodedResult<D>>,
+    Decoded: Into<DecodedResult<E>>,
+{
+    fn from(value: Decoded) -> Self {
+        const LEN: usize = 5;
+        let vec = match value {
+            Decoded::Array(vec) => vec,
+            _ => {
+                return Err(format!(
+                    "can not decode {:?} as a tuple because it is not an array",
+                    value
+                ))
+            }
+        };
+        if vec.len() != LEN {
+            return Err(format!(
+                "{:?} has {} elements instead of {}",
+                vec,
+                vec.len(),
+                LEN
+            ));
+        }
+        let mut iter = vec.into_iter();
+        Ok((
+            Into::<DecodedResult<A>>::into(iter.next().unwrap())?,
+            Into::<DecodedResult<B>>::into(iter.next().unwrap())?,
+            Into::<DecodedResult<C>>::into(iter.next().unwrap())?,
+            Into::<DecodedResult<D>>::into(iter.next().unwrap())?,
+            Into::<DecodedResult<E>>::into(iter.next().unwrap())?,
+        ))
     }
 }
 
@@ -130,26 +312,33 @@ mod json_tests {
     }
 
     #[test]
+    fn can_get_decoded() {
+        assert_decodes_to::<Decoded>(Integer(7), Integer(7));
+        assert_decodes_to::<Decoded>(Text("foo".into()), Text("foo".into()));
+        assert_decodes_to::<Decoded>(Array(vec![]), Array(vec![]));
+    }
+
+    #[test]
     fn can_get_integer() {
-        let i: i64 = -5;
+        let i = -5;
         assert_decodes_to::<i64>(Integer(i), i);
     }
 
     #[test]
     fn can_get_unsigned_from_int() {
-        let u: u64 = 7;
+        let u = 7;
         assert_decodes_to::<u64>(Integer(7), u);
     }
 
     #[test]
     fn can_get_float_from_int() {
-        let f: f64 = 7.0;
+        let f = 7.0;
         assert_decodes_to::<f64>(Integer(7), f);
     }
 
     #[test]
     fn can_get_scalar() {
-        let f: f64 = 7.0;
+        let f = 7.0;
         assert_decodes_to::<f64>(Scalar(f), f);
     }
 
@@ -194,15 +383,15 @@ mod json_tests {
     }
 
     #[test]
-    fn can_get_some_option_decoded() {
-        let i: i64 = 7;
-        assert_decodes_to::<Option<Decoded>>(Integer(7), Some(Integer(i)));
+    fn can_get_some_option() {
+        let i = 7;
+        assert_decodes_to::<Option<i64>>(Integer(7), Some(i));
     }
 
     #[test]
-    fn can_get_none_option_decoded() {
-        let option: Option<Decoded> = None;
-        assert_decodes_to::<Option<Decoded>>(Null, option);
+    fn can_get_none_option() {
+        let option = None;
+        assert_decodes_to::<Option<i64>>(Null, option);
     }
 
     #[test]
@@ -214,14 +403,139 @@ mod json_tests {
     #[test]
     fn can_get_array_of_ints() {
         let values = vec![7, 8, 9];
-        let decodable = Array(values.iter().map(|i| Integer(*i)).collect());
-        let result: Vec<i64> = DecodedResult::<Vec<Decoded>>::from(decodable)
-            .expect("failed to decode as array")
-            .into_iter()
-            .map(|d| {
-                DecodedResult::<i64>::from(d).expect("failed to decode array element as integer")
-            })
-            .collect();
-        assert_eq!(result, values);
+        assert_decodes_to::<Vec<i64>>(Array(vec![Integer(7), Integer(8), Integer(9)]), values);
+    }
+
+    #[test]
+    fn can_get_array_of_options() {
+        let values = vec![Some(7), Some(8), None, Some(10)];
+        assert_decodes_to::<Vec<Option<i64>>>(
+            Array(vec![Integer(7), Integer(8), Null, Integer(10)]),
+            values,
+        );
+    }
+
+    #[test]
+    fn empty_array_is_not_null() {
+        assert_doesnt_decode_to::<()>(Array(vec![]));
+    }
+
+    #[test]
+    fn single_wrong_type_stops_array_from_decoding() {
+        assert_doesnt_decode_to::<Vec<i64>>(Array(vec![Integer(7), Integer(12), Null, Integer(3)]));
+    }
+
+    #[test]
+    fn can_get_one_tuple() {
+        let value = (7,);
+        assert_decodes_to::<(i64,)>(Array(vec![Integer(7)]), value);
+    }
+
+    #[test]
+    fn one_long_array_is_not_inner_value() {
+        assert_doesnt_decode_to::<i64>(Array(vec![Integer(7)]));
+    }
+
+    #[test]
+    fn value_is_not_one_tuple() {
+        assert_doesnt_decode_to::<(i64,)>(Integer(7));
+    }
+
+    #[test]
+    fn can_get_two_tuple() {
+        let value = (7, "hello".into());
+        assert_decodes_to::<(i64, String)>(Array(vec![Integer(7), Text("hello".into())]), value);
+    }
+
+    #[test]
+    fn can_get_three_tuple() {
+        let value = (7, "hello".into(), None);
+        assert_decodes_to::<(i64, String, Option<f64>)>(
+            Array(vec![Integer(7), Text("hello".into()), Null]),
+            value,
+        );
+    }
+
+    #[test]
+    fn can_get_four_tuple() {
+        let value = (7, "hello".into(), None, Point3::new(1.0, 2.0, 3.0));
+        assert_decodes_to::<(i64, String, Option<f64>, Point3<f64>)>(
+            Array(vec![
+                Integer(7),
+                Text("hello".into()),
+                Null,
+                Vector(Vector3::new(1.0, 2.0, 3.0)),
+            ]),
+            value,
+        );
+    }
+
+    #[test]
+    fn can_get_five_tuple() {
+        let value = (7, "hello".into(), None, Point3::new(1.0, 2.0, 3.0), 3.5);
+        assert_decodes_to::<(i64, String, Option<f64>, Point3<f64>, f64)>(
+            Array(vec![
+                Integer(7),
+                Text("hello".into()),
+                Null,
+                Vector(Vector3::new(1.0, 2.0, 3.0)),
+                Scalar(3.5),
+            ]),
+            value,
+        );
+    }
+
+    #[test]
+    fn can_get_tuple_in_tuple() {
+        let value = ((7, 8), vec![]);
+        assert_decodes_to::<((i64, i64), Vec<String>)>(
+            Array(vec![Array(vec![Integer(7), Integer(8)]), Array(vec![])]),
+            value,
+        );
+    }
+
+    #[test]
+    fn can_only_get_one_tuple_if_array_that_size() {
+        let decoded = Array(vec![Null]);
+        assert_doesnt_decode_to::<(Decoded, Decoded)>(decoded.clone());
+        assert_doesnt_decode_to::<(Decoded, Decoded, Decoded)>(decoded.clone());
+        assert_doesnt_decode_to::<(Decoded, Decoded, Decoded, Decoded)>(decoded.clone());
+        assert_doesnt_decode_to::<(Decoded, Decoded, Decoded, Decoded, Decoded)>(decoded.clone());
+    }
+
+    #[test]
+    fn can_only_get_two_tuple_if_array_that_size() {
+        let decoded = Array(vec![Null, Null]);
+        assert_doesnt_decode_to::<(Decoded,)>(decoded.clone());
+        assert_doesnt_decode_to::<(Decoded, Decoded, Decoded)>(decoded.clone());
+        assert_doesnt_decode_to::<(Decoded, Decoded, Decoded, Decoded)>(decoded.clone());
+        assert_doesnt_decode_to::<(Decoded, Decoded, Decoded, Decoded, Decoded)>(decoded.clone());
+    }
+
+    #[test]
+    fn can_only_get_three_tuple_if_array_that_size() {
+        let decoded = Array(vec![Null, Null, Null]);
+        assert_doesnt_decode_to::<(Decoded,)>(decoded.clone());
+        assert_doesnt_decode_to::<(Decoded, Decoded)>(decoded.clone());
+        assert_doesnt_decode_to::<(Decoded, Decoded, Decoded, Decoded)>(decoded.clone());
+        assert_doesnt_decode_to::<(Decoded, Decoded, Decoded, Decoded, Decoded)>(decoded.clone());
+    }
+
+    #[test]
+    fn can_only_get_four_tuple_if_array_that_size() {
+        let decoded = Array(vec![Null, Null, Null, Null]);
+        assert_doesnt_decode_to::<(Decoded,)>(decoded.clone());
+        assert_doesnt_decode_to::<(Decoded, Decoded)>(decoded.clone());
+        assert_doesnt_decode_to::<(Decoded, Decoded, Decoded)>(decoded.clone());
+        assert_doesnt_decode_to::<(Decoded, Decoded, Decoded, Decoded, Decoded)>(decoded.clone());
+    }
+
+    #[test]
+    fn can_only_get_five_tuple_if_array_that_size() {
+        let decoded = Array(vec![Null, Null, Null, Null, Null]);
+        assert_doesnt_decode_to::<(Decoded,)>(decoded.clone());
+        assert_doesnt_decode_to::<(Decoded, Decoded)>(decoded.clone());
+        assert_doesnt_decode_to::<(Decoded, Decoded, Decoded)>(decoded.clone());
+        assert_doesnt_decode_to::<(Decoded, Decoded, Decoded, Decoded)>(decoded.clone());
     }
 }
