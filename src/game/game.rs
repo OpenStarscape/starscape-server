b904a5a48615d86105772c6521e433b1322f1091
Sophie Winter <git@phie.me>
2021-01-03 01:16:00 -0800

    Scaled down solar system by 1000000x to make movement more visible

--- src/game/game.rs
+++ src/game/game.rs
@@ -1,38 +1,44 @@
 use super::*;
 
-pub fn init(state: &mut State) {
-    God::default().install(state);
+fn init_solar_system(state: &mut State, scale: f64) {
+    // Note that scale affects mass, size and position but not velocity. This keeps orbits correct.
 
     // all values are intended to be correct for the sun
     let sun = state.create_entity();
     Body::new()
         .with_class(BodyClass::Celestial)
         .with_position(Point3::origin())
-        .with_sphere_shape(696340.0)
-        .with_mass(1.989e+27)
+        .with_sphere_shape(696340.0 * scale)
+        .with_mass(1.989e+27 * scale)
         .install(state, sun);
 
     // all values are intended to be correct for earth
     let earth = state.create_entity();
-    let earth_pos = Point3::new(1.496e+8, 0.0, 0.0);
+    let earth_pos = Point3::new(1.496e+8, 0.0, 0.0) * scale;
     let earth_vel = Vector3::new(0.0, 0.0, 30.0);
     Body::new()
         .with_class(BodyClass::Celestial)
         .with_position(earth_pos)
         .with_velocity(earth_vel)
-        .with_sphere_shape(6371.0)
-        .with_mass(5.972e+21)
+        .with_sphere_shape(6371.0 * scale)
+        .with_mass(5.972e+21 * scale)
         .install(state, earth);
 
     // all values are intended to be correct for luna (earth's moon)
     let luna = state.create_entity();
     Body::new()
         .with_class(BodyClass::Celestial)
-        .with_position(earth_pos + Vector3::new(3.844e5, 0.0, 0.0))
+        .with_position(earth_pos + Vector3::new(3.844e5, 0.0, 0.0) * scale)
         .with_velocity(earth_vel + Vector3::new(0.0, 0.0, 1.022))
-        .with_sphere_shape(1737.0)
-        .with_mass(7.34767309e19)
+        .with_sphere_shape(1737.0 * scale)
+        .with_mass(7.34767309e19 * scale)
         .install(state, luna);
+}
+
+pub fn init(state: &mut State) {
+    God::default().install(state);
+
+    init_solar_system(state, 0.000001);
 
     /*
     let _ship_a = create_ship(
