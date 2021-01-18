26af39507934a5a87d1be97f1bdb3bf54a3e6d4a
William Wold <wm@wmww.sh>
2021-01-17 16:08:54 -0800

    Websocket: log IP address

--- src/server/websocket/websocket_warp_filter.rs
+++ src/server/websocket/websocket_warp_filter.rs
@@ -5,16 +5,17 @@ pub fn websocket_warp_filter(new_session_tx: Sender<Box<dyn SessionBuilder>>) ->
     // Everything captured by the warp filter needs to be clonable and sync
     let new_session_tx = Arc::new(Mutex::new(new_session_tx));
     warp::path("websocket")
+        .and(warp::addr::remote())
         // The `ws()` filter will prepare the Websocket handshake.
         .and(warp::ws())
-        .map(move |ws: warp::ws::Ws| {
+        .map(move |addr: Option<SocketAddr>, ws: warp::ws::Ws| {
             let new_session_tx = new_session_tx.clone();
             // And then our closure will be called when it completes.
             Box::new(ws.on_upgrade(move |websocket| {
                 if let Err(e) = new_session_tx
                     .lock()
                     .unwrap()
-                    .send(Box::new(WebsocketSessionBuilder::new(websocket)))
+                    .send(Box::new(WebsocketSessionBuilder::new(addr, websocket)))
                 {
                     warn!("creating WebSocket session: {}", e);
                 }
