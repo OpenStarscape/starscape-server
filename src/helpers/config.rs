46470b8fb845946648918b25c22f77d0473d8885
Athena Martin <secure@alm.website>
2021-01-28 19:47:40 -0500

    Allow configuring some Server::new() parameters

--- src/helpers/config.rs
+++ src/helpers/config.rs
@@ -5,6 +5,11 @@ use config::{File, Environment, Config};
 /// Get the current configuration.
 pub fn get() -> Config {
     let mut conf = Config::default();
+    conf.set_default("tcp", true).unwrap();
+    conf.set_default("websockets", true).unwrap();
+    conf.set_default("webrtc", true).unwrap();
+    conf.set_default("https", true).unwrap();
+    conf.set_default("http_content", "../web/dist").unwrap();
     conf.set_default("max_game_time", 1200.0).unwrap();
     conf.merge(File::with_name("starscape")).unwrap().merge(Environment::with_prefix("STARSCAPE")).unwrap();
     return conf;
