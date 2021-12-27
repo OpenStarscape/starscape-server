b08ca6e0621bfefa4a069df98767954bafe03260
William Wold <wm@wmww.sh>
2021-12-27 18:47:58 -0500

    Fix formatting on config entries

--- src/helpers/config/config_entries.rs
+++ src/helpers/config/config_entries.rs
@@ -15,101 +15,158 @@ fn warn_disabled_http_encryption(source: Option<&str>) {
 /// These entries will be applied in order of returned vec (NOT in the order the user specifies the entry). All entries
 /// Will always be applied.
 pub fn config_entries() -> Vec<Box<dyn ConfigEntry>> {
+    // We concat! long strings so the vec can be formatted by rustfmt (see https://github.com/rust-lang/rustfmt/issues/3863)
     vec![
-        <dyn ConfigEntry>::new_bool("tcp", "accept/reject TCP sessions", false, |conf, enable, _| {
-            conf.server.tcp = if enable {
-                Some(SocketAddrConfig::new_loopback())
-            } else {
-                None
-            };
-        }),
-        <dyn ConfigEntry>::new_bool("http_server", "enable/disable the HTTP server (required for WebSockets, WebRTC or the web frontend)", true, |conf, enable, _| {
-            if enable {
-                conf.server.http = Some(HttpServerConfig {
-                    static_content_path: None,
-                    enable_websockets: false,
-                    enable_webrtc_experimental: false,
-                    server_type: HttpServerType::Unencrypted(SocketAddrConfig::new_non_loopback()),
-                });
-            } else {
-                conf.server.http = None;
-            }
-        }),
-        <dyn ConfigEntry>::new_bool("websockets", "", true, |conf, enable, source| {
-            if let Some(http) = &mut conf.server.http {
-                http.enable_websockets = enable;
-            } else {
-                warn_disabled_http(source);
-            }
-        }),
-        <dyn ConfigEntry>::new_bool("webrtc", "accept/reject WebRTC sessions. WARNING: WebRTC sessions may experience bugs due to dropped packets", false, |conf, enable, source| {
-            if let Some(http) = &mut conf.server.http {
-                http.enable_webrtc_experimental = enable;
-            } else {
-                warn_disabled_http(source);
-            }
-        }),
-        <dyn ConfigEntry>::new_bool("https", "enable/disable encryption on the server", false, |conf, enable, source| {
-            if let Some(http) = &mut conf.server.http {
-                http.server_type = if enable {
-                    HttpServerType::Encrypted(HttpsConfig {
-                        socket_addr: SocketAddrConfig::new_non_loopback(),
-                        cert_path: String::new(),
-                        key_path: String::new(),
-                        enable_http_to_https_redirect: true,
-                    })
-                } else {
-                    // TODO: stop conflating unencrypted with loopback
-                    let mut addr = SocketAddrConfig::new_loopback();
-                    addr.port = Some(56_560);
-                    HttpServerType::Unencrypted(addr)
-                }
-            } else {
-                warn_disabled_http(source);
-            }
-        }),
-        <dyn ConfigEntry>::new_string("https_cert_path", "path to the certificate used for HTTPS", "../ssl/cert.pem", |conf, path, source| {
-            if let Some(http) = &mut conf.server.http {
-                if let HttpServerType::Encrypted(https) = &mut http.server_type {
-                    https.cert_path = path;
-                } else {
-                    warn_disabled_http_encryption(source);
-                }
-            } else {
-                warn_disabled_http(source);
-            }
-        }),
-        <dyn ConfigEntry>::new_string("https_key_path", "path to the private key used for HTTPS", "../ssl/privkey.pem", |conf, path, source| {
-            if let Some(http) = &mut conf.server.http {
-                if let HttpServerType::Encrypted(https) = &mut http.server_type {
-                    https.key_path = path;
-                } else {
-                    warn_disabled_http_encryption(source);
-                }
-            } else {
-                warn_disabled_http(source);
-            }
-        }),
-        <dyn ConfigEntry>::new_string("http_content", "path to the directory where static content for the HTTP server is stored (such as the web frontent public/ directory)", "", |conf, path, source| {
-            if let Some(http) = &mut conf.server.http {
-                http.static_content_path = if path.len() > 0 {
-                    Some(path.to_string())
+        <dyn ConfigEntry>::new_bool(
+            "tcp",
+            "accept/reject TCP sessions",
+            false,
+            |conf, enable, _| {
+                conf.server.tcp = if enable {
+                    Some(SocketAddrConfig::new_loopback())
                 } else {
                     None
                 };
-            } else {
-                warn_disabled_http(source);
-            }
-        }),
-        <dyn ConfigEntry>::new_float("max_game_time", "seconds to run the game before exiting, or 0 to run until process is killed", 60.0 * 60.0, |conf, time, source| {
-            if time > 0.0 {
-                conf.max_game_time = Some(time);
-            } else {
-                if time < 0.0 {
-                    warn!("{} should not be negative", source.unwrap());
+            },
+        ),
+        <dyn ConfigEntry>::new_bool(
+            "http_server",
+            concat!(
+                "enable/disable the HTTP server",
+                " (required for WebSockets, WebRTC or the web frontend)"
+            ),
+            true,
+            |conf, enable, _| {
+                if enable {
+                    conf.server.http = Some(HttpServerConfig {
+                        static_content_path: None,
+                        enable_websockets: false,
+                        enable_webrtc_experimental: false,
+                        server_type: HttpServerType::Unencrypted(
+                            SocketAddrConfig::new_non_loopback(),
+                        ),
+                    });
+                } else {
+                    conf.server.http = None;
                 }
-                conf.max_game_time = None;
-            }
-        }),
+            },
+        ),
+        <dyn ConfigEntry>::new_bool(
+            "websockets",
+            "accept/reject WebSocket connections",
+            true,
+            |conf, enable, source| {
+                if let Some(http) = &mut conf.server.http {
+                    http.enable_websockets = enable;
+                } else {
+                    warn_disabled_http(source);
+                }
+            },
+        ),
+        <dyn ConfigEntry>::new_bool(
+            "webrtc",
+            concat!(
+                "accept/reject WebRTC sessions.",
+                " WARNING: WebRTC sessions may experience bugs due to dropped packets"
+            ),
+            false,
+            |conf, enable, source| {
+                if let Some(http) = &mut conf.server.http {
+                    http.enable_webrtc_experimental = enable;
+                } else {
+                    warn_disabled_http(source);
+                }
+            },
+        ),
+        <dyn ConfigEntry>::new_bool(
+            "https",
+            "enable/disable encryption on the server",
+            false,
+            |conf, enable, source| {
+                if let Some(http) = &mut conf.server.http {
+                    http.server_type = if enable {
+                        HttpServerType::Encrypted(HttpsConfig {
+                            socket_addr: SocketAddrConfig::new_non_loopback(),
+                            cert_path: String::new(),
+                            key_path: String::new(),
+                            enable_http_to_https_redirect: true,
+                        })
+                    } else {
+                        // TODO: stop conflating unencrypted with loopback
+                        let mut addr = SocketAddrConfig::new_loopback();
+                        addr.port = Some(56_560);
+                        HttpServerType::Unencrypted(addr)
+                    }
+                } else {
+                    warn_disabled_http(source);
+                }
+            },
+        ),
+        <dyn ConfigEntry>::new_string(
+            "https_cert_path",
+            "path to the certificate used for HTTPS",
+            "../ssl/cert.pem",
+            |conf, path, source| {
+                if let Some(http) = &mut conf.server.http {
+                    if let HttpServerType::Encrypted(https) = &mut http.server_type {
+                        https.cert_path = path;
+                    } else {
+                        warn_disabled_http_encryption(source);
+                    }
+                } else {
+                    warn_disabled_http(source);
+                }
+            },
+        ),
+        <dyn ConfigEntry>::new_string(
+            "https_key_path",
+            "path to the private key used for HTTPS",
+            "../ssl/privkey.pem",
+            |conf, path, source| {
+                if let Some(http) = &mut conf.server.http {
+                    if let HttpServerType::Encrypted(https) = &mut http.server_type {
+                        https.key_path = path;
+                    } else {
+                        warn_disabled_http_encryption(source);
+                    }
+                } else {
+                    warn_disabled_http(source);
+                }
+            },
+        ),
+        <dyn ConfigEntry>::new_string(
+            "http_content",
+            concat!(
+                "path to the directory where static content for the HTTP server is stored",
+                " (such as the web frontent public/ directory)"
+            ),
+            "",
+            |conf, path, source| {
+                if let Some(http) = &mut conf.server.http {
+                    http.static_content_path = if path.len() > 0 {
+                        Some(path.to_string())
+                    } else {
+                        None
+                    };
+                } else {
+                    warn_disabled_http(source);
+                }
+            },
+        ),
+        <dyn ConfigEntry>::new_float(
+            "max_game_time",
+            "seconds to run the game before exiting, or 0 to run until process is killed",
+            60.0 * 60.0,
+            |conf, time, source| {
+                if time > 0.0 {
+                    conf.max_game_time = Some(time);
+                } else {
+                    if time < 0.0 {
+                        warn!("{} should not be negative", source.unwrap());
+                    }
+                    conf.max_game_time = None;
+                }
+            },
+        ),
     ]
 }
