e82595f03a9b4d4de8f8ed24fbed66fc4a943c5d
Sophie Winter <git@phie.me>
2022-01-22 22:19:30 -0500

    Test command line argument parsing

--- src/helpers/config/parse_args.rs
+++ src/helpers/config/parse_args.rs
@@ -110,3 +110,309 @@ pub fn parse_args(builder: &mut ConfigBuilder, args: Vec<String>) -> Result<(), 
     }
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn assert_success(entries: Vec<Box<dyn ConfigEntry>>, args: Vec<&str>) {
+        let mut builder = ConfigBuilder::new(entries);
+        let args = std::iter::once("program_name")
+            .chain(args.into_iter())
+            .map(ToOwned::to_owned)
+            .collect();
+        parse_args(&mut builder, args).unwrap();
+        let mut conf = MasterConfig::default();
+        builder.apply_to(&mut conf).unwrap();
+    }
+
+    fn assert_parse_fail(entries: Vec<Box<dyn ConfigEntry>>, args: Vec<&str>) {
+        let mut builder = ConfigBuilder::new(entries);
+        let args = std::iter::once("program_name")
+            .chain(args.into_iter())
+            .map(ToOwned::to_owned)
+            .collect();
+        parse_args(&mut builder, args).unwrap_err();
+    }
+
+    fn assert_apply_fail(entries: Vec<Box<dyn ConfigEntry>>, args: Vec<&str>) {
+        let mut builder = ConfigBuilder::new(entries);
+        let args = std::iter::once("program_name")
+            .chain(args.into_iter())
+            .map(ToOwned::to_owned)
+            .collect();
+        parse_args(&mut builder, args).unwrap();
+        let mut conf = MasterConfig::default();
+        builder.apply_to(&mut conf).unwrap_err();
+    }
+
+    #[test]
+    fn accepts_no_args() {
+        assert_success(vec![], vec![])
+    }
+
+    #[test]
+    fn accepts_string() {
+        let foo_oneshot = OneshotBeforeDrop::new();
+        assert_success(
+            vec![<dyn ConfigEntry>::new_string(
+                "foo",
+                "",
+                "abc",
+                move |_, value, source| {
+                    assert!(source.is_some());
+                    assert_eq!(value, "xyz");
+                    foo_oneshot.fire();
+                    Ok(())
+                },
+            )],
+            vec!["--foo", "xyz"],
+        )
+    }
+
+    #[test]
+    fn accepts_int() {
+        let foo_oneshot = OneshotBeforeDrop::new();
+        assert_success(
+            vec![<dyn ConfigEntry>::new_int(
+                "foo",
+                "",
+                0,
+                move |_, value, source| {
+                    assert!(source.is_some());
+                    assert_eq!(value, 7);
+                    foo_oneshot.fire();
+                    Ok(())
+                },
+            )],
+            vec!["--foo", "7"],
+        )
+    }
+
+    /*
+    #[test]
+    fn accepts_negative_int() {
+        let foo_oneshot = OneshotBeforeDrop::new();
+        assert_success(
+            vec![
+                <dyn ConfigEntry>::new_int("foo", "", 0, move |_, value, source| {
+                    assert!(source.is_some());
+                    assert_eq!(value, -12);
+                    foo_oneshot.fire();
+                    Ok(())
+                }),
+            ],
+            vec!["--foo", "-12"],
+        )
+    }
+    */
+
+    #[test]
+    fn rejects_invalid_int() {
+        assert_parse_fail(
+            vec![<dyn ConfigEntry>::new_int("foo", "", 0, move |_, _, _| {
+                panic!();
+            })],
+            vec!["--foo", "abc"],
+        )
+    }
+
+    #[test]
+    fn accepts_float() {
+        let foo_oneshot = OneshotBeforeDrop::new();
+        assert_success(
+            vec![<dyn ConfigEntry>::new_float(
+                "foo",
+                "",
+                0.0,
+                move |_, value, source| {
+                    assert!(source.is_some());
+                    assert_eq!(value, 32.5);
+                    foo_oneshot.fire();
+                    Ok(())
+                },
+            )],
+            vec!["--foo", "32.5"],
+        )
+    }
+
+    #[test]
+    fn accepts_int_for_float_entry() {
+        let foo_oneshot = OneshotBeforeDrop::new();
+        assert_success(
+            vec![<dyn ConfigEntry>::new_float(
+                "foo",
+                "",
+                0.0,
+                move |_, value, source| {
+                    assert!(source.is_some());
+                    assert_eq!(value, 32.0);
+                    foo_oneshot.fire();
+                    Ok(())
+                },
+            )],
+            vec!["--foo", "32"],
+        )
+    }
+
+    #[test]
+    fn rejects_invalid_float() {
+        assert_parse_fail(
+            vec![<dyn ConfigEntry>::new_float(
+                "foo",
+                "",
+                0.0,
+                move |_, _, _| {
+                    panic!();
+                },
+            )],
+            vec!["--foo", "abc"],
+        )
+    }
+
+    #[test]
+    fn rejects_float_for_int_entry() {
+        assert_parse_fail(
+            vec![<dyn ConfigEntry>::new_int("foo", "", 0, move |_, _, _| {
+                panic!();
+            })],
+            vec!["--foo", "32.5"],
+        )
+    }
+
+    #[test]
+    fn accepts_multiple() {
+        let foo_oneshot = OneshotBeforeDrop::new();
+        let bar_oneshot = OneshotBeforeDrop::new();
+        assert_success(
+            vec![
+                <dyn ConfigEntry>::new_string("foo", "", "abc", move |_, value, source| {
+                    assert!(source.is_some());
+                    assert_eq!(value, "xyz");
+                    foo_oneshot.fire();
+                    Ok(())
+                }),
+                <dyn ConfigEntry>::new_int("bar", "", 0, move |_, value, source| {
+                    assert!(source.is_some());
+                    assert_eq!(value, 7);
+                    bar_oneshot.fire();
+                    Ok(())
+                }),
+            ],
+            vec!["--bar", "7", "--foo", "xyz"],
+        )
+    }
+
+    #[test]
+    fn accepts_bools() {
+        let foo_oneshot = OneshotBeforeDrop::new();
+        let bar_oneshot = OneshotBeforeDrop::new();
+        assert_success(
+            vec![
+                <dyn ConfigEntry>::new_bool("foo", "", false, move |_, value, source| {
+                    assert!(source.is_some());
+                    assert!(value);
+                    foo_oneshot.fire();
+                    Ok(())
+                }),
+                <dyn ConfigEntry>::new_bool("bar", "", true, move |_, value, source| {
+                    assert!(source.is_some());
+                    assert!(!value);
+                    bar_oneshot.fire();
+                    Ok(())
+                }),
+            ],
+            vec!["--foo", "true", "--bar", "false"],
+        )
+    }
+
+    #[test]
+    fn rejects_invalid_bool_value() {
+        assert_parse_fail(
+            vec![<dyn ConfigEntry>::new_bool(
+                "foo",
+                "",
+                false,
+                move |_, _, _| {
+                    panic!();
+                },
+            )],
+            vec!["--foo", "1"],
+        )
+    }
+
+    #[test]
+    fn accepts_enum() {
+        let foo_oneshot = OneshotBeforeDrop::new();
+        assert_success(
+            vec![<dyn ConfigEntry>::new_enum(
+                "foo",
+                "",
+                vec![
+                    <dyn ConfigEntry>::new_enum_variant("abc", "", |_, _| {
+                        panic!();
+                    }),
+                    <dyn ConfigEntry>::new_enum_variant("xyz", "", move |_, source| {
+                        assert!(source.is_some());
+                        foo_oneshot.fire();
+                    }),
+                    <dyn ConfigEntry>::new_enum_variant("ijk", "", |_, _| {
+                        panic!();
+                    }),
+                ],
+            )],
+            vec!["--foo", "xyz"],
+        )
+    }
+
+    #[test]
+    fn rejects_invalid_enum() {
+        assert_apply_fail(
+            vec![<dyn ConfigEntry>::new_enum(
+                "foo",
+                "",
+                vec![
+                    <dyn ConfigEntry>::new_enum_variant("abc", "", |_, _| {
+                        panic!();
+                    }),
+                    <dyn ConfigEntry>::new_enum_variant("xyz", "", |_, _| {
+                        panic!();
+                    }),
+                    <dyn ConfigEntry>::new_enum_variant("ijk", "", |_, _| {
+                        panic!();
+                    }),
+                ],
+            )],
+            vec!["--foo", "bar"],
+        )
+    }
+
+    #[test]
+    fn accepts_multi_word_name_with_dash() {
+        let foo_oneshot = OneshotBeforeDrop::new();
+        assert_success(
+            vec![<dyn ConfigEntry>::new_int(
+                "foo_bar",
+                "",
+                0,
+                move |_, value, source| {
+                    assert!(source.is_some());
+                    assert_eq!(value, 7);
+                    foo_oneshot.fire();
+                    Ok(())
+                },
+            )],
+            vec!["--foo-bar", "7"],
+        )
+    }
+
+    #[test]
+    fn rejects_underscore_version_of_name() {
+        assert_parse_fail(
+            vec![<dyn ConfigEntry>::new_int("foo_bar", "", 0, move |_, _, _| {
+                panic!();
+            })],
+            vec!["--foo_bar", "32.5"],
+        )
+    }
+}
