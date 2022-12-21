50ee43f8ba635f9354e2cd81aa87c7513e937540
Sophie Winter <wm@wmww.sh>
2022-12-20 19:41:40 -0800

    Update dependencies (except async/network ones)

--- src/engine/component_key.rs
+++ src/engine/component_key.rs
@@ -1,32 +1,14 @@
 use super::*;
 
 /// A handle to a component, only used within the engine module. Outside the engine components are
-/// referred to by their type and EntityKey
-#[derive(Default, Eq, PartialEq, Ord, PartialOrd, Hash)]
+/// referred to by their type and EntityKey. We ise derivitive crate instead of normal derive
+/// because of https://github.com/rust-lang/rust/issues/26925.
 #[repr(transparent)]
+#[derive(derivative::Derivative)]
+#[derivative(Default, Eq, PartialEq, Ord, PartialOrd, Hash, Copy, Debug, Clone)]
 pub struct ComponentKey<T> {
     data: slotmap::KeyData,
     phantom: PhantomData<*const T>,
-}
-
-/// Required because of https://github.com/rust-lang/rust/issues/26925
-impl<T> Clone for ComponentKey<T> {
-    fn clone(&self) -> Self {
-        ComponentKey::<T> {
-            data: self.data,
-            phantom: self.phantom,
-        }
-    }
-}
-
-/// Required because of https://github.com/rust-lang/rust/issues/26925
-impl<T> Copy for ComponentKey<T> {}
-
-/// Required because of https://github.com/rust-lang/rust/issues/26925
-impl<T> std::fmt::Debug for ComponentKey<T> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self.data)
-    }
 }
 
 impl<T> From<slotmap::KeyData> for ComponentKey<T> {
@@ -46,4 +28,9 @@ impl<T> From<ComponentKey<T>> for slotmap::KeyData {
 
 /// All that is required for safety is that "all methods must behave exactly as if we’re operating
 /// on a KeyData directly"
-unsafe impl<T: Sized> slotmap::Key for ComponentKey<T> {}
+unsafe impl<T: Sized> slotmap::Key for ComponentKey<T> {
+    fn data(&self) -> slotmap::KeyData {
+        self.data
+    }
+
+}
